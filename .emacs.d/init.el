de17afa5554603f5ab75e7d65d3e4e0531b325ad
Nikola Brkovic <nikolabrk@protonmail.com>
2026-04-26 23:47:05 +0200

    New emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,18 +1,6 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
 (setq custom-file (concat user-emacs-directory "/custom.el"))
 
@@ -21,7 +9,6 @@
 (tool-bar-mode -1)
 (desktop-save-mode 1)
 (electric-pair-mode 1)
-(savehist-mode 1)
 (recentf-mode 1)
 
 (setq auth-source-save-behavior nil)
@@ -37,51 +24,48 @@
 
 (setq tab-always-indent 'complete)
 (setq text-mode-ispell-word-completion nil)
-
 (setq lexical-binding t)
 
-(let ((path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
-  (setenv "PATH" path)
-  (setq exec-path 
-        (append
-         (split-string-and-unquote path ":")
-         exec-path)))
-
-(set-frame-font "Iosevka 11" nil t)
-
-(setq straight-use-package-by-default t)
-
-(straight-use-package 'use-package)
+;; (set-frame-font "Iosevka 11" nil t)
 
 (use-package catppuccin-theme
+  :ensure t
   :custom
   (catppucin-flavor 'macchiato)
   :init
   (load-theme 'catppuccin))
 
+(use-package project
+  :ensure t)
+
+(use-package transient
+  :ensure t)
+
 (use-package vertico
-  :straight t
+  :ensure t
   :init (vertico-mode))
 
 (use-package savehist
-  :straight t
+  :ensure nil
   :init
   (savehist-mode))
 
 (use-package orderless
-  :straight t
+  :ensure t
   :custom
   (completion-styles '(orderless basic partial-completion))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package marginalia
+  :ensure t
   :bind (:map minibuffer-local-map
 	      ("M-A" . marginalia-cycle))
   :init
   (marginalia-mode))
 
 (use-package consult
+  :ensure t
   :bind
   (
    ("M-s l" . consult-line)
@@ -101,38 +85,49 @@
   (setq consult-narrow-key "<"))
 
 (use-package embark
+  :ensure t
   :bind (("C-." . embark-act)
          :map minibuffer-local-map
          ("C-c C-c" . embark-collect)
          ("C-c C-e" . embark-export)))
 
 (use-package embark-consult
+  :ensure t
   :hook (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package corfu
+  :ensure t
   :custom
   (corfu-auto t)
   (corfu-quit-no-match 'separator)
   :init (global-corfu-mode))
 
-(use-package haskell-mode)
 (use-package paredit
+  :ensure t
   :hook
   (lisp-mode . paredit-mode))
 
 (use-package eglot
+  :ensure nil
   :hook
-  (haskell-mode . 'eglot-ensure)
   (c++-mode . 'eglot-ensure))
 
 (use-package yasnippet
+  :ensure t
   :init
   (yas-global-mode))
 
-(use-package flymake)
+(use-package flymake
+  :ensure t)
 
-(use-package cmake-mode)
-(use-package magit)
+(use-package cmake-mode
+  :ensure t)
+(use-package magit
+  :ensure t)
 (use-package pdf-tools
+  :ensure t
   :config
   (pdf-tools-install))
+
+(use-package realgud
+  :ensure t)
